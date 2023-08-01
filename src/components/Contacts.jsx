b24d2c5cd82164199d2e9f34602fc8587d2dd0cd
Olga-Olga <olga_l@ukr.net>
2023-08-01 20:34:31 +0300

    2 task

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,27 +1,59 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 
 export class Contacts extends React.Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
+    number: '',
+    filter: '',
   };
 
-  handleChangeInput = event => {
+  handleChangeInputName = event => {
     // event.target.value
     this.setState({ name: event.target.value });
   };
 
+  handleChangeInputNumber = event => {
+    // event.target.value
+    this.setState({ number: event.target.value });
+    console.log(event.target.value);
+  };
+
   handleAddContact = () => {
     this.setState(prevState => ({
-      contacts: [...prevState.contacts, prevState.name],
+      contacts: [
+        ...prevState.contacts,
+        { id: nanoid(), name: prevState.name, number: prevState.number },
+      ],
     }));
+    console.log(this.state);
+  };
+
+  handleFilter = event => {
+    // console.log(event.target.value);
+    this.setState({ filter: event.target.value });
+  };
+
+  handleFilterContacts = () => {
+    const fil = this.state.contacts.filter(el => {
+      return el.name.includes(this.state.filter);
+    });
+
+    // console.log(fil);
+    return fil;
   };
 
   render() {
     return (
       <div>
         <input
-          onChange={this.handleChangeInput}
+          onChange={this.handleChangeInputName}
           type="text"
           name="name"
           value={this.state.name}
@@ -29,8 +61,36 @@ export class Contacts extends React.Component {
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
-        <button>Add contact</button>
-        <div>input</div>
+        <input
+          onChange={this.handleChangeInputNumber}
+          type="tel"
+          name="number"
+          value={this.state.number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+        <button onClick={this.handleAddContact}>Add contact</button>
+        <input value={this.state.filter} onChange={this.handleFilter}></input>
+        <ul>
+          {this.state.contacts.map(el => {
+            return (
+              <li key={el.id}>
+                {el.name} {el.number}
+              </li>
+            );
+          })}
+        </ul>
+        <ul>
+          {this.handleFilterContacts().map(el => {
+            return (
+              <li key={el.id}>
+                {el.name} {el.number}
+              </li>
+            );
+          })}
+          {console.log(this.handleFilterContacts())}
+        </ul>
       </div>
     );
   }
